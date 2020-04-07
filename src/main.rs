9f58f100452dd48a3f74d78753c5ae51bf50fd46
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-04-08 00:06:23 +0200

    --manifest-path. json-diagnostic-rendered-ansi,json-diagnostic-short

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,27 @@
 mod diagnostic; use diagnostic::{parse, *};
 use {std::{env, process::{Command, Stdio}, path::Path}, framework::process::Status};
 
-pub struct Tee<R, W> {reader: R, writer: W}
-impl<R: std::io::Read, W: std::io::Write> Tee<R, W> { pub fn new(reader: R, writer: W) -> Self { Self{reader: reader, writer: writer} } }
-impl<R: std::io::Read, W: std::io::Write> std::io::Read for Tee<R, W> {
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        let n = self.reader.read(buf)?;
-        self.writer.write_all(&buf[..n])?;
-        Ok(n)
-    }
-}
-
 fn main() -> Status {
-    let mut child = Command::new("cargo").args(&["run","--message-format","JSON"]).args(std::env::args().skip(1)).stdout(Stdio::piped())/*.stderr(Stdio::null())*/.spawn()?;
-    let mut stdout = std::io::BufReader::new(child.stdout.take().unwrap());
-    'parse: for msg in parse(&mut Tee::new(&mut stdout, std::io::stdout())) {
+    use clap::Clap;
+    #[derive(Clap)] struct Arguments { #[clap(long = "manifest-path")] manifest_path: String }
+    let arguments: Arguments = Arguments::parse();
+    let mut child = Command::new("cargo").args(&["run","--message-format=json-diagnostic-rendered-ansi,json-diagnostic-short"])
+                                .args(std::env::args().skip(1)).stdout(Stdio::piped()).stderr(Stdio::piped()).spawn()?;
+    'parse: for msg in parse(std::io::BufReader::new(child.stdout.as_mut().unwrap())) {
         if let Message::CompilerMessage(CompilerMessage{message: msg, ..}) = msg? {
-            print!("\n{}", msg.message);
+            if msg.message == "aborting due to previous error" { continue; }
+            print!("{}", msg);
             if msg.spans.is_empty() { continue; }
             impl ToString for Span { fn to_string(&self) -> String { format!("{}:{}:{}", self.file_name, self.line_start, self.column_start) } }
             for span in msg.spans {
                 //print!("\n{}", span.file_name);
-                if Path::new(&span.file_name).exists() {
-                    Command::new(env::var("EDITOR")?).arg(span.to_string()).stderr(Stdio::null()).spawn()?;
+                let path = Path::new(&arguments.manifest_path).with_file_name(&span.file_name);
+                if path.exists() {
+                    Command::new(env::var("EDITOR")?).arg(path).stderr(Stdio::null()).spawn()?;
                     break 'parse;
                 }
             }
         }
     }
-    loop {
-        if let Some(status) = child.try_wait()? { return Ok(status).into() }
-        std::io::copy(&mut stdout, &mut std::io::stdout())?;
-    }
+    child.wait()?.into()
 }
