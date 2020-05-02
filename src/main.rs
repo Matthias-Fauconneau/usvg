eb68b974f24066959d415bd1c62ef071ea281127
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-05-02 12:21:53 +0200

    Looks like Diagnostic::Span::file_name is already resolved, no need to do anything >< :)

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,12 @@ use {std::{process::{Command, Stdio, ExitStatus}}, framework::{core::Ok, process
 fn main() -> Status {
     let mut child = Command::new("cargo").args(&["build","--message-format=json-diagnostic-rendered-ansi"/*short*/]).args(std::env::args().skip(1)).stdout(Stdio::piped()).spawn()?;
     for msg in parse(std::io::BufReader::new(child.stdout.take().unwrap())) { match msg? {
-        Message::CompilerMessage(CompilerMessage{target: Target{src_path, ..}, message: Diagnostic{message, spans, rendered, ..}, ..}) => {
+        Message::CompilerMessage(CompilerMessage{message: Diagnostic{message, spans, rendered, ..}, ..}) => {
             let _ = child.kill(); // Kill on first warning/error to save power/heat
             if message == "aborting due to previous error" { continue; }
             eprint!("{}",rendered.ok()?);
             for span in spans {
-                println!("{}:{}:{}", std::path::Path::new(&src_path).with_file_name(&span.file_name).to_str().unwrap(), span.line_start, span.column_start);
-                // Fails on first error/warning
+                println!("{}:{}:{}", span.file_name, span.line_start, span.column_start);
             }
             use std::os::unix::process::ExitStatusExt; return Status::from(ExitStatus::from_raw(-1));
         },
