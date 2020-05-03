64163001acff9c80867754997f44805446ed9dde
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-05-03 02:34:29 +0200

    Fixes

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,17 @@ use {std::{process::{Command, Stdio, ExitStatus}}, framework::{core::Ok, process
 
 // Maps cargo errors/warning to file:line
 fn main() -> Status {
-    let mut child = Command::new("cargo").args(&["build","--message-format=json-diagnostic-rendered-ansi"/*short*/]).args(std::env::args().skip(1)).stdout(Stdio::piped()).spawn()?;
+    let mut child = Command::new("cargo").arg(std::env::args().nth(1).unwrap())
+                                                                    .args(&["--message-format=json-diagnostic-rendered-ansi"/*short*/])
+                                                                    .args(std::env::args().skip(2))
+                                                                    .stdout(Stdio::piped()).spawn()?;
     for msg in parse(std::io::BufReader::new(child.stdout.take().unwrap())) { match msg? {
         Message::CompilerMessage(CompilerMessage{message: Diagnostic{message, spans, rendered, ..}, ..}) => {
             let _ = child.kill(); // Kill on first warning/error to save power/heat
             if message == "aborting due to previous error" { continue; }
             eprint!("{}",rendered.ok()?);
             for span in spans {
-                println!("{}:{}:{}", span.file_name, span.line_start, span.column_start);
+                if std::path::Path::new(&span.file_name).exists() { println!("{}:{}:{}", span.file_name, span.line_start, span.column_start); }
             }
             use std::os::unix::process::ExitStatusExt; return Status::from(ExitStatus::from_raw(-1));
         },
