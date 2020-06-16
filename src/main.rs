af893edf7fbe3a4d89b6818f21f1d99f47824aa1
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-06-17 00:59:59 +0200

    Backport to stable

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
-mod diagnostic; use diagnostic::{parse, *};
-use {std::{process::{Command, Stdio, ExitStatus}}, framework::{error::Ok, process::Status}};
+mod diagnostic;
 
 // Maps cargo errors/warning to file:line
-fn main() -> Status {
-    let mut child = Command::new("cargo").arg(std::env::args().nth(1).unwrap())
-                                                                    .args(&["--message-format=json-diagnostic-rendered-ansi"/*short*/])
-                                                                    .args(std::env::args().skip(2))
+use std::process::{Command, Stdio};
+fn main() -> Result<(),std::io::Error> {
+	let mut child = Command::new("cargo").args(std::env::args().skip(1))
+																	.arg("--message-format=json-diagnostic-rendered-ansi")
                                                                     .stdout(Stdio::piped()).spawn()?;
+	use diagnostic::{parse, Message, CompilerMessage, Diagnostic};
     for msg in parse(std::io::BufReader::new(child.stdout.take().unwrap())) { match msg? {
-        Message::CompilerMessage(CompilerMessage{message: Diagnostic{message, spans, rendered, ..}, ..}) => {
+        Message::CompilerMessage(CompilerMessage{message: Diagnostic{message, spans, rendered: Some(rendered), ..}, ..}) => {
             let _ = child.kill(); // Kill on first warning/error to save power/heat
             if message == "aborting due to previous error" { continue; }
-            eprint!("{}",rendered.ok()?);
+            eprint!("{}", rendered);
             for span in spans {
                 if std::path::Path::new(&span.file_name).exists() { println!("{}:{}:{}", span.file_name, span.line_start, span.column_start); }
             }
-            use std::os::unix::process::ExitStatusExt; return Status::from(ExitStatus::from_raw(-1));
+			std::process::exit(-1);
         },
         _=>{},
     }}
-    child.wait()?.into() // Forward cargo status (success on warnings)
+    std::process::exit(child.wait()?.code().unwrap_or(-1));
 }
